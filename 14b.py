93a50f9767d213cbabc859412fc3ef14934601bf
Jan Drabek <jasiekdrabek@gmail.com>
2024-12-15 11:30:27 +0100

    Update 14b.py

--- 14b.py
+++ 14b.py
@@ -2,8 +2,7 @@ import re
 import sys
 from typing import Iterator
 
-WIDTH = 101
-HEIGHT = 103
+grid_x, grid_y = 100, 102
 
 
 def parse_input(lines: Iterator[str]) -> Iterator[tuple[int, int, int, int]]:
@@ -13,16 +12,16 @@ def parse_input(lines: Iterator[str]) -> Iterator[tuple[int, int, int, int]]:
 
 def simulate_robot(robot: tuple[int, int, int, int], steps: int) -> tuple[int, int]:
     px, py, vx, vy = robot
-    return (px + vx * steps) % WIDTH, (py + vy * steps) % HEIGHT
+    return (px + vx * steps) % grid_x, (py + vy * steps) % grid_y
 
 def find_easter_egg(robots: list[tuple[int, int, int, int]]) -> int:
-    lines = [set((x, y) for x in range(WIDTH)) for y in range(HEIGHT)]
-    for i in range(WIDTH * HEIGHT):
+    lines = [set((x, y) for x in range(grid_x)) for y in range(grid_y)]
+    for i in range(grid_x * grid_y):
         robot_positions = set(simulate_robot(robot, i) for robot in robots)
         max_line, y = max((len(robot_positions & line), y) for y, line in enumerate(lines))
         if max_line >= 30:
             contiguous = 0
-            for x in range(WIDTH):
+            for x in range(grid_x):
                 if (x, y) in robot_positions:
                     contiguous += 1
                 else:
