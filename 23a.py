c03dd41bf36c79233ca77da4939be29b555007f5
Jan Drabek <jasiekdrabek@gmail.com>
2024-12-23 11:14:50 +0100

    faster part 1

--- 23a.py
+++ 23a.py
@@ -1,34 +1,33 @@
+import networkx as nx
 from itertools import combinations
 
-def find_connected_triples(file_path):
+def find_largest_clique_with_t(file_path):
     # Wczytanie danych z pliku
     with open(file_path, 'r') as file:
         connections = [line.strip().split('-') for line in file.readlines()]
     
-    # Tworzenie zbioru połączeń dla szybkiego sprawdzania
-    connection_set = set(tuple(sorted(conn)) for conn in connections)
+    # Tworzenie grafu
+    graph = nx.Graph()
+    graph.add_edges_from(connections)
 
-    # Tworzymy listę unikalnych komputerów
-    computers = set(comp for conn in connections for comp in conn)
-
-    # Sprawdzanie każdej kombinacji trzech komputerów
-    triples = []
-    for triple in combinations(computers, 3):
-        # Jeśli każdy komputer z trójki jest połączony ze sobą
-        if (
-            tuple(sorted([triple[0], triple[1]])) in connection_set and
-            tuple(sorted([triple[0], triple[2]])) in connection_set and
-            tuple(sorted([triple[1], triple[2]])) in connection_set
-        ) and (triple[0].startswith('t') or triple[1].startswith('t') or triple[2].startswith('t')):
-            triples.append(triple)
-
-    return triples
-
+    # Znalezienie wszystkich klik
+    cliques = nx.algorithms.clique.find_cliques(graph)
+    cliques = [clique for clique in cliques if len(clique) >= 3]
+    clique_3 = set()
+    for c in cliques:
+        for (x,y,z) in combinations(c,3):
+            if any(node.startswith('t') for node in [x,y,z]):
+                clique_3.add((x,y,z))
+    
+    return clique_3
 # Ścieżka do pliku
 file_path = '23.txt'
 
-# Znalezienie wszystkich połączonych trójek
-connected_triples = find_connected_triples(file_path)
+# Znalezienie największego zbioru połączonych komputerów (klika z "t")
+clique_3 = find_largest_clique_with_t(file_path)
 
 # Wyświetlenie wyniku
-print(len(connected_triples))
+if clique_3:
+    print(len(clique_3))
+else:
+    print("Nie znaleziono kliki z komputerem zaczynającym się od 't'.")
